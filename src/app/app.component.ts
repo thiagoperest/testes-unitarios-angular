cb1a87ec03120672be1ef61170971d6c80e3502c
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-07-05 08:34:10 -0300

    Cobertura de Testes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'teste-unitario';
+
+  public soma(value1: number, value2: number) {
+    return value1 + value2;
+  }
 }
