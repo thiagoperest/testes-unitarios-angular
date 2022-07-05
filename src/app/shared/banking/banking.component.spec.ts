e356b43174c7b96cd517f6d57c18d168d87f6567
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-07-05 16:48:07 -0300

    Testando Interface

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -36,6 +36,15 @@ describe('BankingComponent', () => {
     expect(component.getCarteira).toEqual(1100);
   });
 
+  it(`(I) setSacar(): shoud transfer poupanca from carteira`, () => {
+    let el = fixture.debugElement.nativeElement;
+
+    el.querySelector('#input-sacar').value = '10';
+    el.querySelector('#sacar').click();
+    fixture.detectChanges();
+    expect(el.querySelector('#get-carteira').textContent).toEqual('110');
+  });
+
   it(`(U) setSacar(): shoud transfer poupanca dont have string (isNaN) or poupanca < value`, () => {
     expect(component.setSacar('string')).not.toBeTruthy();
     expect(component.setSacar('10000')).not.toBeTruthy();
@@ -56,5 +65,16 @@ describe('BankingComponent', () => {
 
     expect(component.getPoupanca).toEqual(1000);
     expect(component.getCarteira).toEqual(100);
+  });
+
+  it(`(I) setDepositar(): shoud transfer carteira from poupanca`, () => {
+    let el = fixture.debugElement.nativeElement;
+
+    el.querySelector('#input-depositar').value = '10';
+    el.querySelector('#depositar').click();
+    fixture.detectChanges();
+
+    expect(el.querySelector('#get-poupanca').textContent).toEqual('1010');
+    expect(component.getCarteira).toEqual(90);
   });
 });
