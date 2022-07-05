7745c2c68d57efd3aac5f0f7704fc79a1a56117f
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-07-05 17:38:10 -0300

    Investiments Component

---
+++ src/app/shared/investiments/list/list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Investiments } from '../model/investiments';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  public investiments: Array<Investiments> = [
+    {
+      name: "Banco +Brasil",
+      value: 100
+    },
+    {
+      name: "New Brazilian",
+      value: 100
+    },
+    {
+      name: "Brazil Express",
+      value: 100
+    },
+    {
+      name: "BR Bank",
+      value: 100
+    },
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
