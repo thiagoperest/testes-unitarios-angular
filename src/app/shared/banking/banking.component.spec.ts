8763d47558da879e91bc72c564467fb69b1db76c
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-07-05 09:45:48 -0300

    Testando a unidade do componente

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -8,9 +8,8 @@ describe('BankingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BankingComponent ]
-    })
-    .compileComponents();
+      declarations: [BankingComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +20,25 @@ describe('BankingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`(U) getPoupanca(): shoud have poupanca = 1000`, () => {
+    expect(component.getPoupanca).toEqual(1000);
+  });
+
+  it(`(U) getCarteira(): shoud have carteira = 100`, () => {
+    expect(component.getCarteira).toEqual(100);
+  });
+
+  it(`(U) setSacar(): shoud transfer poupanca from carteira`, () => {
+    component.setSacar('1000');
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(1100);
+  });
+
+  it(`(U) setDepositar(): shoud transfer carteira from poupanca`, () => {
+    component.setDepositar('100');
+    expect(component.getCarteira).toEqual(0);
+    expect(component.getPoupanca).toEqual(1100);
   });
 });
